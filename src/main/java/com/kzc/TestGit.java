22e5b7b391fb66117490b09b26a557819ff552dd
kzc <361630601@qq.com>
2020-04-08 01:23:33 +0800

    fix

--- src/main/java/com/kzc/TestGit.java
+++ src/main/java/com/kzc/TestGit.java
@@ -3,5 +3,7 @@ package com.kzc;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("testGit");
+        System.out.println("c1");
+        System.out.println("c2");
     }
 }
